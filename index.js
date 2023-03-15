7544e7b5f9c40b8406b7f452d2f16fc0fe5d3cbb
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-15 09:41:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+
+  
+  if(distanceInFeet==199){
+  
+   return 'This one is on me!'
+  
+}else if(distanceInFeet>2000 && distanceInFeet<=2500){
+  return 'I will gladly take your thirty bucks.';
+}else{
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(newCity){
   // Write your code here!
+  if(newCity==='NYC'){
+    return 'Ok, sounds good.'
+
+  }else{
+    return 'No go.'
+  }
+  
+}
+ 
+
+function switchOnCharmFromTip(newTip){
+  // Write your code here!
+  switch(newTip){
+    case 'generous':
+      return 'Thank you so much.'
+  
+  case 'not as generous':
+    return 'Thank you.'
+
+  default:
+      return 'Bye.'
+
+  }
+
 }
